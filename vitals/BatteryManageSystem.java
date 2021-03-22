5e497a796b6342deeac9d006c58f01c0e7f4f7fe
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-03-22 10:00:05 +0530

    Update and rename BMSDriver.java to BatteryManageSystem.java

--- vitals/BatteryManageSystem.java
+++ vitals/BatteryManageSystem.java
@@ -1,12 +1,13 @@
 package vitals;
 
-public class BMSDriver {
+public class BatteryManageSystem {
 	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {       
         BatteryCheck testBattery = new BatteryCheck(temperature, soc, chargeRate);
         return testBattery.isBatteryOk();
     }
     public static void main(String[] args) {
-        assert(batteryIsOk(25 , 15, 0.9f) == false);
+       
+        assert(batteryIsOk(25, 60, 0.8f) == true);
         assert(batteryIsOk(25 , 65, 0.9f) == false);
         assert(batteryIsOk(50, 65, 0.7f) == false);
         assert(batteryIsOk(15, 85, 0.7f) == false);
@@ -15,6 +16,7 @@ public class BMSDriver {
         assert(batteryIsOk(45, 80, 0.8f) == true);
         assert(batteryIsOk(50 , 90, 0.9f) == false);
         assert(batteryIsOk(-1 , 40, 0.6f) == false);
+        
     }
 }
 
